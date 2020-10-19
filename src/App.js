4bed1c037d05be14b7ade029f0417c9b8542cd26
cantasci <cantasci@live.com>
2020-10-19 20:08:05 +0300

    Initial commit
    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import movies from './movies';
+
+console.log(movies);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Film Listesi</h1>
+      <div>{/* TODO: yapmis oldugumuz componenti buraya ekleyelim */}</div>
     </div>
   );
 }
